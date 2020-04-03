993bafba9aa9027918aaab19ce53a158a4415788
Osama Hosameldeen <42114005+mohandesosama@users.noreply.github.com>
2020-04-03 03:12:04 +0300

    updated the bayes algorithm

--- classificatio-python-code/bayes_rule.py
+++ classificatio-python-code/bayes_rule.py
@@ -38,7 +38,7 @@ def calc_field_probs(df,field_name,field_value):
         counter += 1
         probs[dex]=p
     return probs
-    
+
 def user_input(df):
     user_in={}
     # remove the last column, it is the class label
@@ -52,17 +52,9 @@ def user_input(df):
         user_in[col]=field_value
     return user_in
 
-
-if __name__=="__main__":
-    df=pd.read_csv('StudentsDB.csv')
-    df_train = df.iloc[:14]
-    df_test = df.iloc[14:]
-    # we have to remove the last column from the test set
-    # our algorithm will geuess it 
-    df_test_X=df_test.drop(df_test.columns[[-1]],axis=1)
-    df_test_Y=[(t=='yes')*1 for t in list(df_test.iloc[:,-1])]
+def train_and_test(df_train_XY,df_test_X):
     #print(df_test_Y)
-    class_ps=calc_class_probs(df_train)
+    class_ps=calc_class_probs(df_train_XY)
     #us_in=user_input(df)
     # get all probabilities of the fields in one list
     o=[]
@@ -70,7 +62,7 @@ if __name__=="__main__":
         # navigate the test set record by record
         record=df_test_X.iloc[i].to_dict()
         #print(record)
-        ps=[calc_field_probs(df_train,col,record[col]) for col in record.keys()]
+        ps=[calc_field_probs(df_train_XY,col,record[col]) for col in record.keys()]
         # add the prob of yes, no classes
         ps.append(class_ps)
         total_ps={}
@@ -78,8 +70,25 @@ if __name__=="__main__":
         for dic in ps:
             for key in dic:
                 total_ps[key] = total_ps.get(key,1)* dic[key]
-        #print(ps)
+        # return the ouptut as 1 for yes and 0 for no
         o.append((total_ps['yes'] > total_ps['no'])*1)
+        print(record)
+        if total_ps['yes'] > total_ps['no']:
+            print('buys_computer=yes')
+        else:
+            print('buys_computer=no')
+    return o
+
+if __name__=="__main__":
+    df=pd.read_csv('StudentsDB.csv')
+    df_train_XY = df.iloc[:14]
+    df_test = df.iloc[14:]
+    # we have to remove the last column from the test set
+    # our algorithm will geuess it 
+    df_test_X=df_test.drop(df_test.columns[[-1]],axis=1)
+    df_test_Y=[(t=='yes')*1 for t in list(df_test.iloc[:,-1])]
+    
+    o=train_and_test(df_train_XY,df_test_X)
     
     print('Accuracy : ',np.mean([np.array(o) == np.array(df_test_Y)])*100) 
         
